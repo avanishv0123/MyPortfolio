d682cb14d1eba7039713999c18bf23dbb3aaa84e
Avanish <avanishv73@gmail.com>
2023-09-08 22:16:49 +0530

    updated content

--- js/script.js
+++ js/script.js
@@ -67,3 +67,31 @@ window.addEventListener("scroll", () => {
 function connect() {
   alert("Kindly Contact me from the social links given below....");
 }
+
+const button = document.querySelector("#button");
+
+const displayButton = () => {
+  window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+
+    if (window.scrollY > 100) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  });
+};
+
+const scrollToTop = () => {
+  button.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    console.log(event);
+  });
+};
+
+displayButton();
+scrollToTop();
