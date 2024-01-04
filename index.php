266ecce1871e60e0f0bdff191a14d5375e2c5798
Avanish <avanishv73@gmail.com>
2024-01-04 19:37:50 +0530

    removed php email

--- index.php
+++ index.php
@@ -2,223 +2,223 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Avanish Vishwakarma | Home</title>
-  <!-- Link To CSS -->
-  <link rel="stylesheet" href="./css/style.css" />
-  <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
-  <!-- Box Icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
-  <script src="https://kit.fontawesome.com/bf329c35f1.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Avanish Vishwakarma | Home</title>
+    <!-- Link To CSS -->
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
+    <!-- Box Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" />
+    <script src="https://kit.fontawesome.com/bf329c35f1.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-  <!-- Navbar -->
-  <header>
-    <a href="#" class="logo">Avanish Vishwakarma</a>
+    <!-- Navbar -->
+    <header>
+        <a href="#" class="logo">Avanish Vishwakarma</a>
 
-    <div class="bx bx-menu" id="menu-icon"></div>
+        <div class="bx bx-menu" id="menu-icon"></div>
 
-    <ul class="navbar">
-      <li><a href="#home" class="active">Home</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#Skills">Skills</a></li>
-      <li><a href="#projects">Projects</a></li>
-      <li><a href="#contact">Contact</a></li>
-      <div class="bx bx-moon" id="darkmode"></div>
-    </ul>
-  </header>
-  <!-- Home -->
+        <ul class="navbar">
+            <li><a href="#home" class="active">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#Skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <div class="bx bx-moon" id="darkmode"></div>
+        </ul>
+    </header>
+    <!-- Home -->
 
-  <section class="home" id="home">
-    <div class="social">
-      <a href="https://github.com/avanishv0123" target="_blank"><i class="bx bxl-github"></i></a>
+    <section class="home" id="home">
+        <div class="social">
+            <a href="https://github.com/avanishv0123" target="_blank"><i class="bx bxl-github"></i></a>
 
-      <a href="https://www.linkedin.com/in/avanish-vishwakarma-1817b9210/" target="_blank"><i
-          class="bx bxl-linkedin"></i></a>
-      <a href="https://wa.me/917770077872" target="_blank"><i class="bx bxl-whatsapp"></i></a>
-    </div>
-    <div class="home-img">
-      <img src="images/main.jpg" alt="mypic" />
-    </div>
-    <div class="home-text">
-      <span>Hello, I'm</span>
-      <h1>Avanish Vishwakarma</h1>
-      <h2>Frontend Web Developer</h2>
-      <p>
-        I help you build secure, stable, and scalable web applications.
-        I have gained a wealth of experience in the Frontend Web Applications.
-      </p>
-      <a href="#contact" class="btn">Contact Me</a>
-    </div>
-  </section>
-  <!-- About -->
-  <section class="about" id="about">
-    <div class="heading">
-      <h2>About Me</h2>
-      <!-- <span>Introduction</span> -->
-    </div>
-    <!-- About Content -->
-    <div class="about-container">
-      <div class="about-img">
-        <img src="images/about.jpg" alt="" />
-      </div>
-      <div class="about-text">
-        <p>
-          Hello, I am Avanish Radheshyam Vishwakarma a 23-year-old FrontEnd Web Developer which holds 1 year
-          experience
-          as a Wordpress Developer and also worked as a shopify Developer.
-          I have completed my Masters Degree from Patkar Varde College, Goregaon(Mumbai University) with 9.0
-          CGPA in
-          March
-          2023.I did my SSC from Maharashtra State Board with 87 percent in the Year 2016,
-          After that i have Completed my Intermediate from Viva College, Virar and scored 76 percent. After
-          that i went
-          to complete my graduation in Bachelor of Science in information Technology(BscIT) from Viva College,
-          Virar in
-          the year 2018 and I get graduated in the year 2021. Currently i am working at Markweb Solutions as a
-          FrontEnd
-          Web Developer.
-          <br />
-        </p>
-        </p>
-        <button class="white" onclick="location.href='Av Portfolio.pdf'">
-          Download Resume <i class="fa-solid fa-download"></i>
-        </button>
-        <div class="information">
-          <!-- Box 1 -->
-          <div class="info-box">
-            <i class="bx bxs-user"></i>
-            <span>Avanish R. Vishwakarma</span>
-          </div>
-          <!-- Box 2 -->
-          <div class="info-box">
-            <i class="bx bxs-phone"></i>
-            <span>+91 7770077872</span>
-          </div>
-          <!-- Box 3 -->
-          <div class="info-box">
-            <i class="bx bxs-envelope"></i>
-            <span>avanishv73@gmail.com</span>
-          </div>
+            <a href="https://www.linkedin.com/in/avanish-vishwakarma-1817b9210/" target="_blank"><i
+                    class="bx bxl-linkedin"></i></a>
+            <a href="https://wa.me/917770077872" target="_blank"><i class="bx bxl-whatsapp"></i></a>
+        </div>
+        <div class="home-img">
+            <img src="images/main.jpg" alt="mypic" />
+        </div>
+        <div class="home-text">
+            <span>Hello, I'm</span>
+            <h1>Avanish Vishwakarma</h1>
+            <h2>Frontend Web Developer</h2>
+            <p>
+                I help you build secure, stable, and scalable web applications.
+                I have gained a wealth of experience in the Frontend Web Applications.
+            </p>
+            <a href="#contact" class="btn">Contact Me</a>
+        </div>
+    </section>
+    <!-- About -->
+    <section class="about" id="about">
+        <div class="heading">
+            <h2>About Me</h2>
+            <!-- <span>Introduction</span> -->
+        </div>
+        <!-- About Content -->
+        <div class="about-container">
+            <div class="about-img">
+                <img src="images/about.jpg" alt="" />
+            </div>
+            <div class="about-text">
+                <p>
+                    Hello, I am Avanish Radheshyam Vishwakarma a 23-year-old FrontEnd Web Developer which holds 1 year
+                    experience
+                    as a Wordpress Developer and also worked as a shopify Developer.
+                    I have completed my Masters Degree from Patkar Varde College, Goregaon(Mumbai University) with 9.0
+                    CGPA in
+                    March
+                    2023.I did my SSC from Maharashtra State Board with 87 percent in the Year 2016,
+                    After that i have Completed my Intermediate from Viva College, Virar and scored 76 percent. After
+                    that i went
+                    to complete my graduation in Bachelor of Science in information Technology(BscIT) from Viva College,
+                    Virar in
+                    the year 2018 and I get graduated in the year 2021. Currently i am working at Markweb Solutions as a
+                    FrontEnd
+                    Web Developer.
+                    <br />
+                </p>
+                </p>
+                <button class="white" onclick="location.href='Av Portfolio.pdf'">
+                    Download Resume <i class="fa-solid fa-download"></i>
+                </button>
+                <div class="information">
+                    <!-- Box 1 -->
+                    <div class="info-box">
+                        <i class="bx bxs-user"></i>
+                        <span>Avanish R. Vishwakarma</span>
+                    </div>
+                    <!-- Box 2 -->
+                    <div class="info-box">
+                        <i class="bx bxs-phone"></i>
+                        <span>+91 7770077872</span>
+                    </div>
+                    <!-- Box 3 -->
+                    <div class="info-box">
+                        <i class="bx bxs-envelope"></i>
+                        <span>avanishv73@gmail.com</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- Skills -->
+    <section id="Skills">
+        <h4>Explore My</h4>
+        <h2>Skills</h2>
+        <div class="skill">
+            <div class="details-container1">
+                <h6 class="devhead">FrontEnd Development</h6>
+                <h3><i class="fa-brands fa-html5"></i> HTML</h3>
+                <h3><i class="fa-brands fa-css3-alt"></i> CSS</h3>
+                <h3><i class="fa-brands fa-js"></i> JavaScript</h3>
+                <h3><i class="fa-brands fa-jQuery"></i> jQuery</h3>
+                <h3><i class="fa-brands fa-bootstrap"></i> BootStrap</h3>
+            </div>
+            <div class="details-container2">
+                <h6 class="devhead">BackEnd Development</h6>
+                <h3><i class="fa-brands fa-wordpress"></i> WordPress</h3>
+                <h3><i class=" fa-brands fa-shopify"></i> Shopify</h3>
+                <h3><i class="fa-brands fa-php fa-brands"></i> PHP</h3>
+                <h3><i class=" fa-solid fa-server"></i> MySQL</h3>
+                <h3><i class="fa-brands fa-git"></i> GIT</h3>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Projects  -->
+    <section class="projects" id="projects">
+        <div class="heading">
+            <h2>Projects</h2>
+            <span>My Recent Work</span>
         </div>
 
-      </div>
+        <div class="portfolio-content">
+            <div class="portfolio-img">
+                <a href="https://orthopath.com.au/" target="_blank">
+                    <img src="images/projects/orthopath.jpg" alt="" />
+                </a>
+                <a class="visitBtn" href="https://orthopath.com.au/" target="_blank">Visit Site</a>
+            </div>
+            <div class="portfolio-img">
+                <a href="http://shloka700.com/" target="_blank">
+                    <img src="images/projects/shloka.jpg" alt="" />
+                </a>
+                <a class="visitBtn" href="http://shloka700.com/" target="_blank">Visit Site</a>
+            </div>
+            <div class="portfolio-img">
+                <a href="https://staging.suvidya.ac.in/" target="_blank">
+                    <img src="images/projects/suvidya.jpg" alt="" /></a>
+                <a class="visitBtn" href="https://staging.suvidya.ac.in/" target="_blank">Visit Site</a>
+            </div>
+            <div class="portfolio-img">
+                <a href="https://aligner32.com/pages/test" target="_blank">
+                    <img src="images/projects/aligner32.jpg" alt="" /></a>
+                <a class="visitBtn" href="https://aligner32.com/pages/test" target="_blank">Visit Site</a>
+            </div>
+            <div class="portfolio-img">
+                <a href="https://alignerco.ca/" target="_blank">
+                    <img src="images/projects/alignerco ca.jpg" alt="" /></a>
+                <a class="visitBtn" href="https://alignerco.ca/" target="_blank">Visit Site</a>
+            </div>
+            <div class="portfolio-img">
+                <a href="https://smilepath.com.au/" target="_blank">
+                    <img src="images/projects/smilepath aus.jpg" alt="" /></a>
+                <a class="visitBtn" href="https://smilepath.com.au/" target="_blank">Visit Site</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact  -->
+    <section class="contact" id="contact">
+        <div class="heading">
+            <h2>Contact Me</h2>
+        </div>
+        <div class="contact-form">
+            <form method="POST">
+                <input type="text" placeholder="Your Name" name="name" />
+                <input type="email" name="email" id="" placeholder="Your Email" />
+                <textarea name="msgbox" id="" cols="30" rows="10" placeholder="Write Message Here..."></textarea>
+                <input type="submit" value="Send" class="contact-button" name="sendbtn">
+            </form>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <div class="footer" id="contact">
+        <h2>Connect Me On</h2>
+        <div class="footer-social">
+            <a href="https://wa.me/917770077872" target="_blank"><i class="bx bxl-whatsapp"></i></a>
+            <a href="https://github.com/avanishv0123" target="_blank"><i class="bx bxl-github"></i></a>
+            <a href="https://www.facebook.com/avanishv73/" target="_blank"><i class="bx bxl-facebook"></i></a>
+
+            <a href="https://instagram.com/avanish_vish20" target="_blank"><i class="bx bxl-instagram"></i></a>
+            <a href="https://www.linkedin.com/in/avanish-vishwakarma-1817b9210/"><i class="bx bxl-linkedin"></i></a>
+        </div>
     </div>
-  </section>
-  <!-- Skills -->
-  <section id="Skills">
-    <h4>Explore My</h4>
-    <h2>Skills</h2>
-    <div class="skill">
-      <div class="details-container1">
-        <h6 class="devhead">FrontEnd Development</h6>
-        <h3><i class="fa-brands fa-html5"></i> HTML</h3>
-        <h3><i class="fa-brands fa-css3-alt"></i> CSS</h3>
-        <h3><i class="fa-brands fa-js"></i> JavaScript</h3>
-        <h3><i class="fa-brands fa-jQuery"></i> jQuery</h3>
-        <h3><i class="fa-brands fa-bootstrap"></i> BootStrap</h3>
-      </div>
-      <div class="details-container2">
-        <h6 class="devhead">BackEnd Development</h6>
-        <h3><i class="fa-brands fa-wordpress"></i> WordPress</h3>
-        <h3><i class=" fa-brands fa-shopify"></i> Shopify</h3>
-        <h3><i class="fa-brands fa-php fa-brands"></i> PHP</h3>
-        <h3><i class=" fa-solid fa-server"></i> MySQL</h3>
-        <h3><i class="fa-brands fa-git"></i> GIT</h3>
-
-      </div>
+    <!-- Copyright -->
+    <div class="copyright">
+        <p>
+            Created By
+            <a href="https://www.linkedin.com/in/avanish-vishwakarma-1817b9210/">Avanish Vishwakarma
+            </a>
+            <br>&copy; All Right Reserved.
+        </p>
     </div>
-  </section>
+    <a id="button"><i class="	fas fa-chevron-circle-up"></i></a>
 
-  <!-- Projects  -->
-  <section class="projects" id="projects">
-    <div class="heading">
-      <h2>Projects</h2>
-      <span>My Recent Work</span>
-    </div>
+    <!-- Link To JS -->
+    <script src="./js/script.js"></script>
 
-    <div class="portfolio-content">
-      <div class="portfolio-img">
-        <a href="https://orthopath.com.au/" target="_blank">
-          <img src="images/projects/orthopath.jpg" alt="" />
-        </a>
-        <a class="visitBtn" href="https://orthopath.com.au/" target="_blank">Visit Site</a>
-      </div>
-      <div class="portfolio-img">
-        <a href="http://shloka700.com/" target="_blank">
-          <img src="images/projects/shloka.jpg" alt="" />
-        </a>
-        <a class="visitBtn" href="http://shloka700.com/" target="_blank">Visit Site</a>
-      </div>
-      <div class="portfolio-img">
-        <a href="https://staging.suvidya.ac.in/" target="_blank">
-          <img src="images/projects/suvidya.jpg" alt="" /></a>
-        <a class="visitBtn" href="https://staging.suvidya.ac.in/" target="_blank">Visit Site</a>
-      </div>
-      <div class="portfolio-img">
-        <a href="https://aligner32.com/pages/test" target="_blank">
-          <img src="images/projects/aligner32.jpg" alt="" /></a>
-        <a class="visitBtn" href="https://aligner32.com/pages/test" target="_blank">Visit Site</a>
-      </div>
-      <div class="portfolio-img">
-        <a href="https://alignerco.ca/" target="_blank">
-          <img src="images/projects/alignerco ca.jpg" alt="" /></a>
-        <a class="visitBtn" href="https://alignerco.ca/" target="_blank">Visit Site</a>
-      </div>
-      <div class="portfolio-img">
-        <a href="https://smilepath.com.au/" target="_blank">
-          <img src="images/projects/smilepath aus.jpg" alt="" /></a>
-        <a class="visitBtn" href="https://smilepath.com.au/" target="_blank">Visit Site</a>
-      </div>
-    </div>
-  </section>
-
-  <!-- Contact  -->
-  <section class="contact" id="contact">
-    <div class="heading">
-      <h2>Contact Me</h2>
-    </div>
-    <div class="contact-form">
-      <form method="POST">
-        <input type="text" placeholder="Your Name" name="name" />
-        <input type="email" name="email" id="" placeholder="Your Email" />
-        <textarea name="msgbox" id="" cols="30" rows="10" placeholder="Write Message Here..."></textarea>
-        <input type="submit" value="Send" class="contact-button" name="sendbtn">
-      </form>
-    </div>
-  </section>
-
-  <!-- Footer -->
-  <div class="footer" id="contact">
-    <h2>Connect Me On</h2>
-    <div class="footer-social">
-      <a href="https://wa.me/917770077872" target="_blank"><i class="bx bxl-whatsapp"></i></a>
-      <a href="https://github.com/avanishv0123" target="_blank"><i class="bx bxl-github"></i></a>
-      <a href="https://www.facebook.com/avanishv73/" target="_blank"><i class="bx bxl-facebook"></i></a>
-
-      <a href="https://instagram.com/avanish_vish20" target="_blank"><i class="bx bxl-instagram"></i></a>
-      <a href="https://www.linkedin.com/in/avanish-vishwakarma-1817b9210/"><i class="bx bxl-linkedin"></i></a>
-    </div>
-  </div>
-  <!-- Copyright -->
-  <div class="copyright">
-    <p>
-      Created By
-      <a href="https://www.linkedin.com/in/avanish-vishwakarma-1817b9210/">Avanish Vishwakarma
-      </a>
-      <br>&copy; All Right Reserved.
-    </p>
-  </div>
-  <a id="button"><i class="	fas fa-chevron-circle-up"></i></a>
-
-  <!-- Link To JS -->
-  <script src="./js/script.js"></script>
-
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9751879421694604"
-    crossorigin="anonymous"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9751879421694604"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
@@ -281,6 +281,7 @@ if (isset($_POST['sendbtn'])) {
 
     $mail->send();
     echo '<div class="success">Message has been sent</div>';
+    echo '<script> alert("succcess"></script>'
 
   } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
